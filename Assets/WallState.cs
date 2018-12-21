b1630faff7464bab7f9eae99f929af13ab14aa82
asmallshellscript <rene.stakenborg@gmail.com>
2018-12-20 22:35:05 -0500

    * ADDED png reader and test 1 box wall

---
+++ Assets/WallState.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WallState : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        foreach (var block in GetComponentsInChildren<BlockState>()) {
+            if (block.IsSatisfied() == false)
+                return;
+        }
+        foreach (var celebrate in GetComponentsInChildren<Celebrate>()){
+            celebrate.Confetti();
+        }
+    }
+}
